1be769efb23a92851a7f9bf74bc1305b08fb6457
Santiago Hoyos <santiagojhoyoss@gmail.com>
2026-05-18 22:43:49 -0500

    refactor: remove User module and related files to streamline application structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,12 @@ import { AppController } from './app.controller.js';
 import { AuthModule } from './auth/auth.module.js';
 import { DoctorsModule } from './doctors/doctors.module.js';
 import { PrismaModule } from './prisma/prisma.module.js';
-import { UserModule } from './user/user.module.js';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     AuthModule,
-    UserModule,
     DoctorsModule,
     AppointmentsModule,
   ],
